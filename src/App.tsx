365974bdc8c9957eb055e26a4042088734013cea
Mohit Gupta <mohitgupta@Mohits-MacBook-Pro-1621.local>
2022-11-19 13:17:17 -0600

    Structured the front-end for all the branches

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './Front-End/assets/logo.svg';
+import './Front-End/scss/App.scss';
 
 function App() {
   return (
