6d1c354a64770ed8643218af5022c3999f43f6e6
Amadeus Justinn Haryanto <amadeusjustinn@gmail.com>
2022-11-21 07:44:43 +0000

    Merge front-end into master

--- src/Front-End/pages/01_log_in.tsx
+++ src/Front-End/pages/01_log_in.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import '../scss/App.scss';
 import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faMicrosoft,
+    faGoogle,
+    faFacebook,
+} from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
 function LogInView() {
     const [email, setEmail] = useState<string>("");
@@ -25,18 +33,39 @@ function LogInView() {
         <React.Fragment>
 
             <section className="section-01-log-in">
-                <div id="login-in-box">
+                <div id="log-in-box">
+                    <h1>Log In</h1>
+                    <h5>With your email</h5>
                     <div id="default-log-in">
                         <form onSubmit={tryLogIn}>
-                            <input type="text" name="email" onChange={handleInputChange} value={email}/>
-                            <input type="text" name="password" onChange={handleInputChange} value={password}/>
-                            <input type="submit"/>
+                            <input type="text" name="email" onChange={handleInputChange}
+                                value={email} placeholder="Enter email" />
+                            <input type="text" name="password" onChange={handleInputChange}
+                                value={password} placeholder="Enter password" />
+                            <input type="submit" value="Log In" id="log-in-submit" />
                         </form>
                     </div>
-                    <div id="provider-log-in">
 
+                    <h5>Or with a provider</h5>
+                    <div id="provider-log-in">
+                        <button id="microsoft-log-in">
+                            <FontAwesomeIcon icon={faMicrosoft} /> Microsoft
+                        </button>
+                        <button id="google-log-in">
+                            <FontAwesomeIcon icon={faGoogle} /> Google
+                        </button>
+                        <button id="facebook-log-in">
+                            <FontAwesomeIcon icon={faFacebook} /> Facebook
+                        </button>
                     </div>
                 </div>
+
+                <h5 className="line-break">
+                    <span>Or</span>
+                </h5>
+                <button id="switch-to-sign-up">
+                    <Link to="/sign-up">Sign Up</Link>
+                </button>
             </section>
 
         </React.Fragment>
