61633448485011942d035757494b2123d72a3326
Amadeus Justinn Haryanto <amadeusjustinn@gmail.com>
2022-11-21 07:41:44 +0000

    Merge 01-log-in with front-end

--- src/Front-End/pages/02_sign_up.tsx
+++ src/Front-End/pages/02_sign_up.tsx
@@ -42,26 +42,42 @@ function SignUpView() {
 
             <section className="section-02-sign-up">
                 <div id="sign-up-box">
-                    <h1>Quick Sign Up</h1>
+                    <h1>Sign Up</h1>
                     <h5>With your email</h5>
                     <div id="default-sign-up">
                         <form onSubmit={trySignUp}>
-                            <input type="text" name="email" onChange={handleInputChange} value={email} placeholder="Enter email"/>
-                            <input type="text" name="password" onChange={handleInputChange} value={password} placeholder="Enter password"/>
+                            <input type="text" name="email" onChange={handleInputChange}
+                                value={email} placeholder="Enter email" />
+                            <input type="text" name="password" onChange={handleInputChange}
+                                value={password} placeholder="Enter password" />
                             <h6>Use 7-27 characters with at least one letter and number.</h6>
-                            <input type="text" name="confirm-password" onChange={handleInputChange} value={confirmPassword} placeholder="Confirm password"/>
-                            <input type="submit" value="Create Account" id="sign-up-submit"/>
+                            <input type="text" name="confirm-password" onChange={handleInputChange}
+                                value={confirmPassword} placeholder="Confirm password" />
+                            <input type="submit" value="Create Account" id="sign-up-submit" />
                         </form>
                     </div>
+
                     <h5>Or with a provider</h5>
+
                     <div id="provider-sign-up">
-                        <button id="microsoft-sign-up"><FontAwesomeIcon icon={faMicrosoft}/> Microsoft</button>
-                        <button id="google-sign-up"><FontAwesomeIcon icon={faGoogle}/> Google</button>
-                        <button id="facebook-sign-up"><FontAwesomeIcon icon={faFacebook}/> Facebook</button>
+                        <button id="microsoft-sign-up">
+                            <FontAwesomeIcon icon={faMicrosoft} /> Microsoft
+                        </button>
+                        <button id="google-sign-up">
+                            <FontAwesomeIcon icon={faGoogle} /> Google
+                        </button>
+                        <button id="facebook-sign-up">
+                            <FontAwesomeIcon icon={faFacebook} /> Facebook
+                        </button>
                     </div>
                 </div>
-                <h5 className="line-break"><span>Or</span></h5>
-                <button id="switch-to-log-in"> <Link to="/log-in">Log In</Link></button>
+
+                <h5 className="line-break">
+                    <span>Or</span>
+                </h5>
+                <button id="switch-to-log-in">
+                    <Link to="/log-in">Log In</Link>
+                </button>
             </section>
 
         </React.Fragment>
