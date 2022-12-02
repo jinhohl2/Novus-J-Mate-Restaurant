15ef41734efa637f69978799152ec69be2011f02
Eric Jin <ericjin2@illinois.edu>
2022-12-01 18:13:49 -0600

    Draft of structure of details page

--- src/Front-End/pages/07_restaurant_details.tsx
+++ src/Front-End/pages/07_restaurant_details.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import { randomUUID } from 'crypto';
+import React, { useState } from 'react';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 const RestaurantDetails = () => {
+    const [tabKey, setTabKey] = useState<string>("ratingsReviewsKey");
+    const [code, SetCode] = useState<string>("");
+
     const menu_items = [{
         name: "raw chicken leg",
         about_dish: "straight from the factory farms of Urbana",
@@ -8,6 +14,10 @@ const RestaurantDetails = () => {
         rating_self: 1,
         rating_others: 4
     }];
+
+    function getCode() {
+        SetCode(crypto.randomUUID());
+    }
 
     return (
         <React.Fragment>
@@ -19,54 +29,62 @@ const RestaurantDetails = () => {
                     <h5>Get directions</h5>
                     <a href="google.com">Website</a>
                 </div>
-
-                <div id='details-div'>
-                    <div id='coupon'>
-                        <h2>Unique one-time coupons for you:</h2>
-                    </div>
-
-                    <div id='ratings-and-reviews'>
+                <div id="restaurant-details-div">
+                <Tabs id="restaurant-details-tabs" activeKey={tabKey} onSelect={(tabKey) => setTabKey(tabKey ? tabKey : "ratingsReviewsKey")}>
+                    <Tab className="restaurant-details-tab" eventKey="ratingsReviewsKey" title="Ratings and Reviews">
                         <div id='ratings-self'></div>
                         <div id='ratings-others'></div>
-                    </div>
+                    </Tab>
 
-                    <div id='menu-details'>
-                        {menu_items.map((item) => {
-                            return <div>
-                                <h3>{item.name}</h3>
-                                <h5>{item.about_dish}</h5>
-                                <h4>{item.price}</h4>
-                                <h5>Rating by me: {item.rating_self}</h5>
-                                <h5>Ratings by others: {item.rating_others}</h5>
-                            </div>
-                        })}
-                    </div>
-
-                    <div id='notes-and-reviews-by-self'>
-                        <div id='notes-by-self'>
-                            <h2>Private notes (Visible only to self)</h2>
-
-                            <div id='notes-buttons'>
-                                <button className='edit'>Edit</button>
-                                <button className='cancel'>Cancel</button>
-                                <button className='submit'>Submit</button>
-                            </div>
-
-                            <textarea id="notes-by-self-textarea" cols={30} rows={10}></textarea>
+                    <Tab className="restaurant-details-tab" eventKey="menuKey" title="Menu">
+                        <div id='menu-details'>
+                            {menu_items.map((item) => {
+                                return <div>
+                                    <h3>{item.name}</h3>
+                                    <h5>{item.about_dish}</h5>
+                                    <h4>{item.price}</h4>
+                                    <h5>Rating by me: {item.rating_self}</h5>
+                                    <h5>Ratings by others: {item.rating_others}</h5>
+                                </div>
+                            })}
                         </div>
+                    </Tab>
 
-                        <div id='ratings-after-visit'>
-                            <h2>Submit ratings after visit</h2>
+                    <Tab className="restaurant-details-tab" eventKey="notesReviewsKey" title="Own Notes and Reviews">
+                        <div id='notes-and-reviews-by-self'>
+                            <div id='notes-by-self'>
+                                <h2>Private notes (Visible only to self)</h2>
 
-                            <div id='reviews-buttons'>
-                                <button className='edit'>Edit</button>
-                                <button className='cancel'>Cancel</button>
-                                <button className='submit'>Submit</button>
+                                <div id='notes-buttons'>
+                                    <button className='edit'>Edit</button>
+                                    <button className='cancel'>Cancel</button>
+                                    <button className='submit'>Submit</button>
+                                </div>
+
+                                <textarea id="notes-by-self-textarea" cols={30} rows={10}></textarea>
                             </div>
 
-                            <textarea id="reviews-by-self-textarea" cols={30} rows={10}></textarea>
+                            <div id='ratings-after-visit'>
+                                <h2>Submit ratings after visit</h2>
+
+                                <div id='reviews-buttons'>
+                                    <button className='edit'>Edit</button>
+                                    <button className='cancel'>Cancel</button>
+                                    <button className='submit'>Submit</button>
+                                </div>
+
+                                <textarea id="reviews-by-self-textarea" cols={30} rows={10}></textarea>
+                            </div>
                         </div>
-                    </div>
+                    </Tab>
+
+                    <Tab className="restaurant-details-tab" eventKey="couponKey" title="Coupons">
+                        <div id='coupon'>
+                            <button onClick={getCode}>Generate Code</button>
+                            <h3>{code}</h3>
+                        </div>
+                    </Tab>
+                </Tabs>
                 </div>
             </section>
         </React.Fragment>
